792b2509035dad42aa9d218c09e4004189ae4f82
Santiago Esteva <santiagoesteva@gmail.com>
2023-06-09 16:28:24 +0200

    setup storybook

---
+++ stories/Button.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Button, buttonVariants } from "../components/ui/button"
+
+const meta: Meta<typeof Button> = {
+  title: 'Atoms/Button',
+  component: Button,
+  tags: ['autodocs'],
+  parameters: {
+
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+const commonArgs = {
+  children: "Create"
+}
+
+export const Default: Story = {
+  args: {
+    variant: 'default',
+    size: "default",
+    ...commonArgs
+
+  },
+};
